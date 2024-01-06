a037853969247a840aa465d5adf7415a2728fdb6
Rafal <rmarkowski97@gmail.com>
2024-01-06 17:38:14 +0100

    0.0.1 version

---
+++ src/main/java/comparator/configuration/ComparatorApplication.java
@@ -0,0 +1,18 @@
+package comparator.configuration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.invoke.MethodHandles;
+
+@SpringBootApplication
+public class ComparatorApplication {
+    private static final Logger log = LogManager.getLogger(MethodHandles.Lookup.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComparatorApplication.class, args);
+        log.info("Testing log which informs that Comparator Application has started successfully.");
+    }
+}
